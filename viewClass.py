0bd15277fe759de4e75eb6414e530941ad953126
evnsok <124316988+evnsok@users.noreply.github.com>
2023-02-15 15:26:55 -0500

    fixed title

--- viewClass.py
+++ viewClass.py
@@ -5,6 +5,7 @@ SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 500, 600
 
 # Screen Color
 screenColor = (190, 172, 158)
+rectColor = (210, 195, 179)
 
 pygame.init()
 
@@ -13,5 +14,5 @@ surface = pygame.display.set_mode(SCREEN_SIZE)
 #creating a Title
 def render_title():
   font = pygame.font.Font(None,100)
-  title = font.render('TITLE',True,(0,0,0))
-  surface.blit(title,(140,20))
+  title = font.render('2048',True,(0,0,0))
+  surface.blit(title,(170,20))
